391268b7b57396ed6338915c9d57a0a5e839e4b3
Jidde Koekoek <jiddekoekoek@macbook-pro-van-jidde.local>
2016-02-03 15:35:55 +0100

    Basic app

--- AppStudio1/ViewController.swift
+++ AppStudio1/ViewController.swift
@@ -7,9 +7,12 @@
 //
 
 import UIKit
+import AudioToolbox.AudioServices
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var heartImageView: UIImageView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -19,7 +22,11 @@ class ViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
 
-
+    @IBAction func vibrateOnTap(sender: AnyObject) {
+        AudioServicesPlayAlertSound(SystemSoundID(kSystemSoundID_Vibrate))
+    }
+    
 }
 
